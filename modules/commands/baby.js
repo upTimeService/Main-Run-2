bc681a075c26a41e01aa9fc4214a86e55411f544
tanvir0FS <110109590+tanvir0FS@users.noreply.github.com>
2024-11-13 15:36:20 +0600

    Update baby.js

--- modules/commands/baby.js
+++ modules/commands/baby.js
@@ -13,8 +13,8 @@ module.exports.config = {
   hasPermission: 0,
   description: "better than all sim simi",
   commandCategory: "chat",
-  usePrefix: true,
-  prefix: true,
+  usePrefix: false,
+  prefix: false,
   usages: `[anyMessage] OR\nteach [YourMessage] - [Reply1], [Reply2], [Reply3]... OR\nteach [react] [YourMessage] - [react1], [react2], [react3]... OR\nremove [YourMessage] OR\nrm [YourMessage] - [indexNumber] OR\nmsg [YourMessage] OR\nlist OR\nall OR\nedit [YourMessage] - [NewMessage]`,
 };
 
