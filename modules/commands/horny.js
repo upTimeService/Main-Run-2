d5e9ff20925a9f8840204dfc6636f0e420c62bfd
tanvir0FS <110109590+tanvir0FS@users.noreply.github.com>
2024-11-13 16:39:50 +0600

    Update and rename horny1.js to horny.js

---
+++ modules/commands/horny.js
@@ -0,0 +1,27 @@
+const axios = require("axios");
+
+      module.exports.config = {
+      name: "horny",
+      author: "Tanvir Tamim", 
+      commandCategory: "video",
+      category: "xok",
+      prefix: true, 
+      usePrefix: true,
+      hasPermssion: 0,
+
+      };
+      module.exports.run = async function ({api, event, args }) {
+const tanvir143 = args.join(" ");
+         try {
+      api.sendMessage("[🥵] 𝘍𝘪𝘯𝘥𝘪𝘯𝘨 𝘩𝘰𝘳𝘯𝘺 𝘷𝘪𝘥𝘦𝘰...",event.threadID, event.messageID);
+      const {data} = await axios.get(`https://joshweb.click/aigen?prompt=${tanvir143}`)
+      const tanvir = data.result;
+      const stream = await axios.get(tanvir,{responseType: "stream"})
+         api.sendMessage({attachment:stream.data}, event.threadID, event.messageID);
+
+      }
+   catch(tanvir) {
+      api.sendMessage(`error:  ${tanvir.message}`,event.threadID, event.messageID)
+
+   }
+      }
